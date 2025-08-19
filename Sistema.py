2fff5084fd2ab1e0995aa83c06983c898ca2efd9
Wadls <wadsondaniel9@gmail.com>
2025-08-19 05:54:50 -0300

    Inserção função conta e cadastro de usuario

--- Sistema.py
+++ Sistema.py
@@ -1,5 +1,10 @@
+
+
 def extrato():
     i = 0
+    print('''\n
+    +=========== EXTRATO ===========+
+''')
     for index in operacoes_bancarias:
         i+=1
         if index > 0:
@@ -8,8 +13,10 @@ def extrato():
             print(f'- Operação {i}, saque de -R${(index*-1):.2f} ')
     if not operacoes_bancarias:
         print('Lista Vazia')
-    
-             
+    print('''\n
+    +===============================+
+''')
+               
 def deposito(valor):
     operacoes_bancarias.append(valor)
    
@@ -20,20 +27,66 @@ def saque(*,saldo, saque_diario, Limite_Saque, Conta):
     else:
         return False
 
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+def listar_contas(contas):
     
+    for i in contas:
+        for j in i.items():
+            print(j)
+
+
+
 menu = '''\n
 =========== MENU ===========
-[D] Depósito
-[S] Saque
-[E] Extrato
+[D]     Depósito
+[S]     Saque
+[E]     Extrato
+[Nc]    Nova Conta
+[Nu]    Novo Usuário
+[Nl]    Nova Conta
+[Lc]    Listar Contas
 [B] Saída
 ============================
 \n\n'''
+LIMITE_SAQUE = 500
+AGENCIA = '0001'
 
 operacoes_bancarias = []
+usuarios = []
+contas = []
 conta = 1000
 saque_diario = 3
-LIMITE_SAQUE = 500
 
 while True:
     switch = input(menu)
@@ -53,8 +106,19 @@ while True:
     elif switch.lower() == 'e':
         extrato()
         print(f'\n Valor Atual em conta: {conta}')
+    elif switch.lower() == 'nu':
+        criar_usuario(usuarios)    
+    elif switch.lower() == 'nc':
+        numero_conta = len(contas) + 1
+        conta_criada = criar_conta(AGENCIA,numero_conta,usuarios)
+        if conta_criada:
+            contas.append(conta_criada)
+    
+    elif switch.lower() == 'lc':
+        listar_contas(contas)
     elif switch.lower() == 'b':
         break
+
     else:
         print('Entrada Não Reconhecida')
         
--- features.py
+++
@@ -1,6 +0,0 @@
-diconario = dict()
-def criar_usuario(nome, data_de_nascimento,cpf,endereço):
-    diconario[nome] = {data_de_nascimento,cpf,endereço}
-
-criar_usuario('JULIO','12/08/2024','6541325455',42, 'teste',12,131,32,1,132,31,1231,1312)
-print(diconario)
